cba77096dd44791585d741940349ef1d9689449f
Yuns <yuns.xie@qq.com>
2024-07-09 21:26:25 +0800

    ♻️ chore: update dynamic config

--- src/app/streaming/components/Data/index.tsx
+++ src/app/streaming/components/Data/index.tsx
@@ -2,9 +2,6 @@ import https from 'node:https'
 
 import fetch from 'node-fetch'
 
-// eslint-disable-next-line react-refresh/only-export-components
-export const dynamic = 'force-dynamic'
-
 // ref: https://github.com/node-fetch/node-fetch/issues/568#issuecomment-932200523
 const agent = new https.Agent({
   rejectUnauthorized: false,
--- src/app/streaming/page.tsx
+++ src/app/streaming/page.tsx
@@ -4,6 +4,10 @@ import { Counter } from './components/Counter'
 import Data from './components/Data'
 import Loading from './loading'
 
+// ref: https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config
+// eslint-disable-next-line react-refresh/only-export-components
+export const dynamic = 'force-dynamic'
+
 export default async function Page() {
   return (
     <div className='bg-slate-300 p-4'>
